49ac645d3c984641919b17696cf583dc718c97a8
Dustin Sallings <dustin@spy.net>
2012-05-29 15:57:21 -0700

    Added some common data handling utils.

---
+++ datalib/fields.go
@@ -0,0 +1,91 @@
+// Tools for handling github data.
+package githubdata
+
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"log"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type fileData struct {
+	Filename  string    `json:"_id"`
+	Timestamp time.Time `json:"ts"`
+	Type      string    `json:"type"`
+}
+
+func maybeFatal(err error, msg ...string) {
+	if err != nil {
+		if len(msg) > 0 {
+			log.Fatalf("Error: %v (%v)", err, msg)
+		} else {
+			log.Fatalf("Error:  %v", err)
+		}
+	}
+}
+
+func Dochash(doc map[string]interface{}) string {
+	h := fnv.New64()
+	e := json.NewEncoder(h)
+	err := e.Encode(doc)
+	maybeFatal(err, "Problem encoding doc for hash")
+	return fmt.Sprintf("%x", h.Sum64())
+}
+
+func GenerateId(doc map[string]interface{}) (rv string) {
+	parts := []string{}
+	ts := doc["created_at"].(string)
+	t, err := ParseDate(ts)
+	maybeFatal(err)
+
+	parts = append(parts, t.Format("2006-01-02T15-04-05"))
+
+	parts = append(parts, Dochash(doc))
+
+	rv = strings.Join(parts, "-")
+
+	return
+}
+
+func ParseDate(s string) (time.Time, error) {
+	formats := []string{
+		"2006/01/02 15:04:05 -0700",
+		"2006-01-02T15:04:05Z",
+	}
+	for _, f := range formats {
+		t, err := time.Parse(f, s)
+		if err == nil {
+			return t, nil
+		}
+	}
+	return time.Time{}, fmt.Errorf("Could not parse %v", s)
+}
+
+func GetDayOfWeek(doc map[string]interface{}) string {
+	date, err := ParseDate(doc["created_at"].(string))
+	maybeFatal(err)
+	return date.Format("Monday")
+}
+
+func ExplodeDate(doc map[string]interface{}) []int {
+	date, err := ParseDate(doc["created_at"].(string))
+	maybeFatal(err)
+	s := date.Format("2006 01 02 15 04 05")
+	sexp := strings.Split(s, " ")
+	rv := make([]int, 0, len(sexp))
+	for _, part := range sexp {
+		i, err := strconv.ParseInt(part, 10, 32)
+		maybeFatal(err)
+		rv = append(rv, int(i))
+	}
+	return rv
+}
+
+func UpdateWithCustomFields(doc map[string]interface{}) {
+	doc["_id"] = GenerateId(doc)
+	doc["dow"] = GetDayOfWeek(doc)
+	doc["expldate"] = ExplodeDate(doc)
+}
---
+++ datalib/id_test.go
@@ -0,0 +1,114 @@
+package githubdata
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+var sample = []byte(`{
+   "actor": "bobkarreman",
+   "actor_attributes": {
+       "gravatar_id": "d4ce883db263cb0eb6d8f4e9a10efa1c",
+       "login": "bobkarreman",
+       "name": "Bob Karreman",
+       "type": "User"
+   },
+   "created_at": "2012/04/05 03:40:13 -0700",
+   "payload": {
+       "head": "284f9a67ac271074c799eff58f7ae0cd8a980adf",
+       "ref": "refs/heads/develop",
+       "shas": [
+           [
+               "284f9a67ac271074c799eff58f7ae0cd8a980adf",
+               "bob@karreman.org",
+               "Add event handler in pubsub for messge_event and fixed node to be Optional in create node",
+               "Bob Karreman",
+               true
+           ]
+       ],
+       "size": 1
+   },
+   "public": true,
+   "repository": {
+       "created_at": "2011/12/19 04:40:59 -0800",
+       "description": "Implementation of XEP-0080",
+       "fork": true,
+       "forks": 0,
+       "has_downloads": true,
+       "has_issues": false,
+       "has_wiki": true,
+       "homepage": "",
+       "language": "Python",
+       "name": "SleekXMPP--XEP-0080-",
+       "open_issues": 0,
+       "organization": "calendar42",
+       "owner": "calendar42",
+       "private": false,
+       "pushed_at": "2012/04/05 03:40:12 -0700",
+       "size": 124,
+       "url": "https://github.com/calendar42/SleekXMPP--XEP-0080-",
+       "watchers": 1
+   },
+   "type": "PushEvent",
+   "url": "https://github.com/calendar42/SleekXMPP--XEP-0080-/compare/3163bd1177...284f9a67ac"
+}`)
+
+func getDoc(t *testing.T) map[string]interface{} {
+	doc := map[string]interface{}{}
+	err := json.Unmarshal(sample, &doc)
+	if err != nil {
+		t.Errorf("Error unmarshaling sample json: %v", err)
+	}
+	return doc
+}
+
+func TestIDGeneration(t *testing.T) {
+	doc := getDoc(t)
+	got := GenerateId(doc)
+	expected := "2012-04-05T03-40-13-d28d2e69c64055e"
+	if got != expected {
+		t.Fatalf("Expected `%v', got `%v'", expected, got)
+	}
+
+}
+
+type timeTestData struct {
+	Expected string
+	Input    string
+}
+
+var testData = []timeTestData{
+	{"2012 05 29 22 45 20", "2012-05-29T22:45:20Z"},
+	{"2012 04 05 03 40 12", "2012/04/05 03:40:12 -0700"},
+}
+
+func TestFormats(t *testing.T) {
+	for _, ttd := range testData {
+		date, err := ParseDate(ttd.Input)
+		s := date.Format("2006 01 02 15 04 05")
+		if err == nil && s != ttd.Expected {
+			t.Fatalf("Expected %v for %v, got %v",
+				ttd.Expected, ttd.Input, s)
+		}
+	}
+}
+
+func TestSupplementalFields(t *testing.T) {
+	doc := getDoc(t)
+	dow := GetDayOfWeek(doc)
+	if dow != "Thursday" {
+		t.Fatalf("Expected Tuesday, got %v", dow)
+	}
+	got := ExplodeDate(doc)
+	exp := []int{2012, 4, 5, 3, 40, 13}
+	if len(got) != len(exp) {
+		t.Fatalf("Expected %d items, got %d - %v",
+			len(exp), len(got), got)
+	}
+	for i := range exp {
+		if exp[i] != got[i] {
+			t.Fatalf("Expected %d, got %d at %d: %v / %v",
+				exp[i], got[i], i, exp, got)
+		}
+	}
+}
